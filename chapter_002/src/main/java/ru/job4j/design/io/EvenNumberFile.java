909093c52dba3cdd51d1d87a58da2a2a21b65e94
mikhail43435 <68198830+mikhail43435@users.noreply.github.com>
2020-11-20 20:11:14 +0300

    FileInputStream [#391252]

---
+++ chapter_002/src/main/java/ru/job4j/design/io/EvenNumberFile.java
@@ -0,0 +1,30 @@
+package ru.job4j.design.io;
+
+import java.io.FileInputStream;
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        StringBuilder text = new StringBuilder();
+        try (FileInputStream in = new FileInputStream("C:\\projects\\job4j_elementary\\even.txt")) {
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String string = new String(text);
+        string = string.replaceAll("\r\n"," ");
+        String[] array = string.split(" ");
+        ToIntFunction<String> toIntFunction = Integer::parseInt;
+        Function<Integer, String> func =
+                e -> (e % 2) != 0 ? "The digit " + e + " is odd" : "The digit " + e + " is even";
+        Arrays.stream(array).
+                mapToInt(toIntFunction).
+                mapToObj(func::apply).
+                forEach(System.out::println);
+    }
+}
