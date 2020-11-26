95861f41311ea9ac073edea5d956a76e8248465c
mikhail43435 <68198830+mikhail43435@users.noreply.github.com>
2020-11-26 18:20:33 +0300

    Именованные аргументы [#391246]

---
+++ chapter_002/src/main/java/ru/job4j/design/io/ArgsName.java
@@ -0,0 +1,46 @@
+package ru.job4j.design.io;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        return values.get(key);
+    }
+
+    private void parse(String[] args) throws IllegalArgumentException {
+       if (args.length == 0) {
+           throw new IllegalArgumentException("Invalid value of parameter. "
+                   + "Parameter show have format -KEY=VALUE");
+       }
+        for (String line : args) {
+            String[] pair = line.split("=");
+            if (pair.length < 2) {
+                throw new IllegalArgumentException("Invalid value of parameter. "
+                        + "Parameter show have format -KEY=VALUE");
+            }
+            if (!pair[0].startsWith("-") || pair[0].length() < 2) {
+                throw new IllegalArgumentException("Invalid key. Key should starts with"
+                        + " dash symbol and has at least one letter after");
+            }
+            values.put(pair[0].substring(1), pair[1]);
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/design/io/ArgsNameTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.design.io;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+public class ArgsNameTest {
+
+    @Test
+    public void whenGetFirst() {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        assertThat(jvm.get("Xmx"), is("512"));
+        assertThat(jvm.get("encoding"), is("UTF-8"));
+    }
+
+    @Test
+    public void whenGetFirstReorder() {
+        ArgsName jvm = ArgsName.of(new String[] {"-encoding=UTF-8", "-Xmx=512"});
+        assertThat(jvm.get("Xmx"), is("512"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenGetNotExist() {
+        ArgsName jvm = ArgsName.of(new String[] {});
+        jvm.get("Xmx");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenDashIsMissing() {
+        ArgsName jvm = ArgsName.of(new String[] {"encoding=UTF-8", "-Xmx=512"});
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenKeyIsMissing() {
+        ArgsName jvm = ArgsName.of(new String[] {"=UTF-8", "-Xmx=512"});
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenValueIsMissing() {
+        ArgsName jvm = ArgsName.of(new String[] {"=UTF-8", "-Xmx="});
+    }
+}
