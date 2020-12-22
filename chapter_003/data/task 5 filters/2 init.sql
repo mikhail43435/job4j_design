42ff60ee7d0a1ea22f9b3703925dd87a7acbae5e
mikhail43435 <68198830+mikhail43435@users.noreply.github.com>
2020-12-22 20:22:53 +0300

    Фильтры [#391176]

---
+++ chapter_003/data/task 5 filters/2 init.sql
@@ -0,0 +1,23 @@
+insert into type(name) values ('молоко');
+insert into type(name) values ('сыр');
+insert into type(name) values ('колбаса');
+insert into type(name) values ('консервы');
+insert into type(name) values ('творог');
+insert into type(name) values ('мороженное');
+
+insert into product(name, expired_date, price, type_id) values ('Правильного молоко', '01.01.2021', 145, 1);
+insert into product(name, expired_date, price, type_id) values ('Домик в деревне', '11.02.2021', 54, 1);
+insert into product(name, expired_date, price, type_id) values ('Валио', '25.04.2021', 87, 1);
+insert into product(name, expired_date, price, type_id) values ('сыр Валио', '25.03.2021', 578, 2);
+insert into product(name, expired_date, price, type_id) values ('сыр пошехонский', '25.03.2021', 532, 2);
+insert into product(name, expired_date, price, type_id) values ('сервелад краковский', '25.02.2021', 832, 3);
+insert into product(name, expired_date, price, type_id) values ('хамон', '25.02.2021', 2498, 3);
+insert into product(name, expired_date, price, type_id) values ('сайра', '23.01.2021', 45, 4);
+insert into product(name, expired_date, price, type_id) values ('домик в деревне творог', '23.04.2021', 78, 5);
+insert into product(name, expired_date, price, type_id) values ('Домик в деревне', '11.02.2021', 54, 1);
+insert into product(name, expired_date, price, type_id) values ('Валио', '25.04.2021', 87, 1);
+insert into product(name, expired_date, price, type_id) values ('сыр Валио', '25.03.2021', 578, 2);
+insert into product(name, expired_date, price, type_id) values ('мор Валио', '25.03.2021', 43, 6);
+insert into product(name, expired_date, price, type_id) values ('фав стаканчик', '25.03.2021', 53, 6);
+insert into product(name, expired_date, price, type_id) values ('Мороженное Валио', '25.03.2021', 43, 6);
+insert into product(name, expired_date, price, type_id) values ('Мороженное фав стаканчик', '25.03.2021', 53, 6);
