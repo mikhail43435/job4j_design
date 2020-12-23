149008c8078432f46828bd506dcb4edd3b6643c1
mikhail43435 <68198830+mikhail43435@users.noreply.github.com>
2020-12-23 15:21:57 +0300

    Видео [#391180]

---
+++ chapter_003/data/task 6 outer join/2 init.sql
@@ -0,0 +1,21 @@
+insert into departments(name) values ('accounting');
+insert into departments(name) values ('sales');
+insert into departments(name) values ('r&d');
+insert into departments(name) values ('top managers');
+insert into departments(name) values ('customer support');
+insert into departments(name) values ('marketing');
+
+
+insert into emploees(name, departments_id) values ('DF', 1);
+insert into emploees(name, departments_id) values ('FD', 2);
+insert into emploees(name, departments_id) values ('EE', 3);
+insert into emploees(name, departments_id) values ('JO', 4);
+insert into emploees(name, departments_id) values ('WB', 5);
+insert into emploees(name, departments_id) values ('VE', 5);
+insert into emploees(name, departments_id) values ('GE', 3);
+insert into emploees(name, departments_id) values ('WA', 3);
+insert into emploees(name, departments_id) values ('OF', 2);
+insert into emploees(name, departments_id) values ('VE', 2);
+insert into emploees(name, departments_id) values ('FE', null);
+insert into emploees(name, departments_id) values ('PS', null);
+
