3f4fc887aac312a712f3509c6f20474228ae02f0
mikhail43435 <68198830+mikhail43435@users.noreply.github.com>
2021-03-04 16:19:59 +0300

    Шаблонизатор. [#391269]

---
+++ chapter_005/src/main/java/ru/job4j/design/tdd/template/GeneratorBasic.java
@@ -0,0 +1,10 @@
+package ru.job4j.design.tdd.template;
+
+import java.util.Map;
+
+public class GeneratorBasic implements Generator {
+    @Override
+    public String produce(String template, Map<String, String> args) {
+        return null;
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/design/tdd/template/GeneratorBasicTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.design.tdd.template;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GeneratorBasicTest {
+
+    @Test
+    public void testThenBasicTest() {
+        GeneratorBasic generator = new GeneratorBasic();
+        String stringIn = "I am a ${name}, Who are ${subject}";
+        String stringOut = "I am Petr, I am 31 years old";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Petr");
+        map.put("age", "31");
+        assertThat(generator.produce(stringIn, map), is(stringOut));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testThenKeyNotFound() {
+        GeneratorBasic generator = new GeneratorBasic();
+        String stringIn = "I am ${name}, I am ${subject} years old";
+        String stringOut = "I am Petr, I am 31 years old";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Petr");
+        generator.produce(stringIn, map);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testThenKeyNotExist() {
+        GeneratorBasic generator = new GeneratorBasic();
+        String stringIn = "I am ${name}";
+        String stringOut = "I am Petr";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Petr");
+        map.put("age", "31");
+        generator.produce(stringIn, map);
+    }
+}
